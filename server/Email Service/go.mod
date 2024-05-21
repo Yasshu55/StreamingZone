8a89f73f6e36b2686d1da22e85e9144d04a71797
Yashwanth sai <20r91a0530@tkrec.ac.in>
2024-05-21 18:55:27 +0530

    Implemented the basic functionality of sending mails through kafka

--- server/Email Service/go.mod
+++ server/Email Service/go.mod
@@ -3,6 +3,30 @@ module Email-Service
 go 1.22.3
 
 require (
-	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
-	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
+	github.com/Shopify/sarama v1.43.2
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 )
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/eapache/go-resiliency v1.6.0 // indirect
+	github.com/eapache/go-xerial-snappy v0.0.0-20230731223053-c322873962e3 // indirect
+	github.com/eapache/queue v1.1.0 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-uuid v1.0.3 // indirect
+	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
+	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
+	github.com/jcmturner/gofork v1.7.6 // indirect
+	github.com/jcmturner/gokrb5/v8 v8.4.4 // indirect
+	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
+	github.com/klauspost/compress v1.17.8 // indirect
+	github.com/pierrec/lz4/v4 v4.1.21 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+)
+
+replace github.com/Shopify/sarama => github.com/IBM/sarama v1.43.2
--- server/Email Service/main.go
+++ server/Email Service/main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"os"
+	"log"
 
-	"gopkg.in/gomail.v2"
+	"github.com/Shopify/sarama"
+	gomail "gopkg.in/gomail.v2"
 )
 
-func sendMail() {
+type LivestreamEvent struct {
+	Event     string `json:"event"`
+	UserEmail string `json:"userEmail"`
+	Timestamp string `json:"timestamp"`
+}
+
+func sendMail(userMail string) {
 	m := gomail.NewMessage()
-	m.SetHeader("From", os.Getenv("SMTP_USERNAME"))
-	m.SetHeader("To", "yashwanthsaichennupalli@gmail.com")
+	// m.SetHeader("From", os.Getenv("SMTP_USERNAME"))
+	m.SetHeader("From", "ysgaming2003@gmail.com")
+	m.SetHeader("To", userMail)
 	m.SetHeader("Subject", "Test Mail from GO")
 	m.SetBody("text/plain", "This is a test email sent from a Go application.")
 
-	d := gomail.NewDialer("smtp.gmail.com", 587, os.Getenv("SMTP_USERNAME"), os.Getenv("SMTP_PASSWORD"))
+	d := gomail.NewDialer("smtp.gmail.com", 587, "ysgaming2003@gmail.com", "lvbq koun arsl tpln")
 
 	if err := d.DialAndSend(m); err != nil {
 		fmt.Println("Error sending email:", err)
@@ -24,5 +33,43 @@ func sendMail() {
 }
 
 func main() {
-	sendMail()
+	// Set up the Sarama configuration
+	config := sarama.NewConfig()
+	config.Consumer.Return.Errors = true
+
+	// Initialize the Sarama consumer
+	consumer, err := sarama.NewConsumer([]string{"localhost:9092"}, config)
+	if err != nil {
+		log.Fatal("Error creating consumer:", err)
+	}
+	defer consumer.Close()
+
+	// Consume the "livestream-events" topic
+	partitionConsumer, err := consumer.ConsumePartition("livestream-events", 0, sarama.OffsetNewest)
+	if err != nil {
+		log.Fatal("Error creating partition consumer:", err)
+	}
+
+	defer partitionConsumer.Close()
+
+	fmt.Println("Kafka consumer has started, waiting for messages...")
+
+	for {
+		select {
+		case msg := <-partitionConsumer.Messages():
+			var event LivestreamEvent
+			err := json.Unmarshal(msg.Value, &event)
+			if err != nil {
+				log.Println("Error unmarshalling event:", err)
+				continue
+			}
+
+			if event.Event == "STARTED" {
+				sendMail(event.UserEmail)
+			}
+
+		case err := <-partitionConsumer.Errors():
+			log.Println("Error in partition consumer:", err)
+		}
+	}
 }
