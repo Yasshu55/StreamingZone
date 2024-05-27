486218a4b63a97851ff2a8d63e25c3a8eacd3c3d
Yashwanth sai <20r91a0530@tkrec.ac.in>
2024-05-28 00:02:38 +0530

    Changed file name - p -> P

--- server/Live Streaming/routes/authRoutes.js
+++ server/Live Streaming/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import PlatformSelection from "../platformSelection.js";
+import PlatformSelection from "../PlatformSelection.js";
 const router = Router()
 
 router.post("/videoStreaming",PlatformSelection.videoPlatforms)
